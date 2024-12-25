dc0bdbe766056e50f38c178bb5ff66e8bc2881fa
Borislav2-0 <crva20@mail.ru>
2024-12-25 13:52:00 +0300

    Домашняя работа по уроку "Пространство имён"

---
+++ module_3_1.py
@@ -0,0 +1,24 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    len_str, up_str, low_str = len(string), string.upper(), string.lower()
+    count_calls()
+    return (len_str, up_str, low_str)
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.lower()
+    for item in list_to_search:
+        if string == item.lower():
+            return True
+    return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
